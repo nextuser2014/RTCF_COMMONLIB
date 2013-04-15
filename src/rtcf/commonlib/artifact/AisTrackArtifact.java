d3cd016d992426c1a02d9ffd730a9d79e430bd72
Stefano Barbati <stefano.barbati@next.it>
2013-04-15 18:15:17 +0200

    adeguamento aistopic artifact alla irs

--- src/rtcf/commonlib/artifact/AisTrackArtifact.java
+++ src/rtcf/commonlib/artifact/AisTrackArtifact.java
@@ -1,21 +1,20 @@
 package rtcf.commonlib.artifact;
 
-import java.util.ArrayList;
-
 import rtcf.commonlib.dto.rtcf.AisTrackRtcf;
 
 public class AisTrackArtifact extends RtcfTopicArtifact {
 
-	private static final long serialVersionUID = -5609391972011734619L;
-	private ArrayList<AisTrackRtcf> aisTrack;
+	private static final long serialVersionUID = -2914140224440614660L;
+	
+	private AisTrackRtcf aisTrack;
 
 	//CTOR
-	public AisTrackArtifact (int id, ArrayList<AisTrackRtcf> aisTrack) {
+	public AisTrackArtifact (int id, AisTrackRtcf aisTrack) {
 		super(id);
 		this.aisTrack = aisTrack;
 	}
 
-	public ArrayList<AisTrackRtcf> getAisTracks() {
+	public AisTrackRtcf getAisTracks() {
 		return aisTrack;
 	}
 	
